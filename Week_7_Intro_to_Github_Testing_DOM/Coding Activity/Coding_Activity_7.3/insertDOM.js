50901aecad20a3b3bb5db26ccc3cfceb2b33afdf
Jegan_Karthi <125629033+jegankarthi@users.noreply.github.com>
2023-06-30 18:56:54 +0530

    Add files via upload

---
+++ Week_7_Intro_to_Github_Testing_DOM/Coding Activity/Coding_Activity_7.3/insertDOM.js
@@ -0,0 +1,16 @@
+function addItem() {
+  // TODO: add 'item' to the list of TODOs
+
+  //Step 1: identify the value of the myInput element. 
+
+  //Step 2: Create a text node containing that value
+
+  //Step 3: Create a new li element and append the text node to it
+
+  //Step 4: Append the li element to the existing myTODOs element. 
+}
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = addItem;
+}
---
+++ Week_8_Styles_&_Bootstrap/Coding_Activity/Coding_Activity_8.3/grid.js
@@ -0,0 +1,21 @@
+let position = 1; // The starting position of the grid
+const gridSize = 100; // The total number of the grid cells
+
+function createGrid() {
+  // Should create a new Div based on the gridSize variable value and append these divs to the element with id "target"
+}
+
+function move() {
+  // Should increment the position variable by 1 each 100ms taking into consideration the gridSize variable value
+  // This function should make use of the toggle function below to change the CSS class on a specific div element
+}
+
+function toggle(position) {
+  // Takes a position parameter referencing a grid cell and sets the class name of that cell (or div) to the class 'on'
+  // The CSS class "on" is defined in the styles.css file
+}
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = { createGrid, move, toggle };
+}
